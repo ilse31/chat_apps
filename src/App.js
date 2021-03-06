61f9bdf18a2ab9f32c7000286b613925f65d8aca
ilse31 <ilhamprazsetyo33@gmail.com>
2021-03-06 21:47:51 +0700

    fixing

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import './App.css'
 import React from 'react';
 
 function App(props) {
+    const projectID = '4b2e4854-8465-46cd-9b34-39962716c0fd';
     return (
         <div>
             <ChatEngine
                 height="100vh"
-                projectID="4b2e4854-8465-46cd-9b34-39962716c0fd"
+                projectID={projectID}
                 userName="ilham"
                 userSecret="12345"
                 renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
