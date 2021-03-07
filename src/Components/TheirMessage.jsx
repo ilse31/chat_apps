9f065826e12feccd2ad1ead193f6fb32820e7fed
ilse31 <ilhamprazsetyo33@gmail.com>
2021-03-07 09:24:35 +0700

    tambah

--- src/Components/TheirMessage.jsx
+++ src/Components/TheirMessage.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 const TheirMessage = ({ lastMessage, message }) => {
     const isFiristMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
     return (
-        <div>
+        <div className="message-row">
+            {isFiristMessageByUser && (
+                <div
+                    className="message-avatar"
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+            {message?.attachment?.lenght > 0
+                ? (
+                    <img
+                        src={message.attachment[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFiristMessageByUser ? '4px' : '48px' }} />
+                ) : (
+                    <div className="message" style={{ float: 'left', backgroundColor: '#3B2A50' }}>
+                        {message.text}
+                    </div>
+                )
+
+            }
 
         </div>
     )
